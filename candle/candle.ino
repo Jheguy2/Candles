f522eb1156ce08da22bb87daa856c83f8b659756
Jack Eller <jheguy2@gmail.com>
2017-10-18 23:26:12 -0400

    The candles work, but the sensitivity is really fucked

--- candle/candle.ino
+++ candle/candle.ino
@@ -1,37 +1,72 @@
-boolean blow = false;
-boolean light = false;
-boolean blowstill = false;
+int amountOfBlowingA0 = 0;
+int amountOfBlowingA1 = 0;
+int amountOfBlowingA2 = 0;
+int amountOfBlowingA3 = 0;
+boolean lightForA0 = false;
+boolean lightForA1 = false;
+boolean lightForA2 = false;
+boolean lightForA3 = false;
+
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(12, INPUT);
-  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(A3, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A1, INPUT);
   pinMode(A0, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
 
+  Serial.println(amountOfBlowingA3);
+  
+  amountOfBlowingA0 = analogRead(A0);
+  amountOfBlowingA1 = analogRead(A1);
+  amountOfBlowingA2 = analogRead(A2);
+  amountOfBlowingA3 = analogRead(A3);    
+  
+  if(amountOfBlowingA0 < 600){//check to see if a0 is being blown ;)
+    lightForA0 = !lightForA0;
+  }
+  
+  if(amountOfBlowingA1 < 600){//check to see if a1 is being blown ;)
+    lightForA1 = !lightForA1;
+  }
 
-blow = digitalRead(12);
+  if(amountOfBlowingA2 < 600){//check to see if a2 is being blown ;)
+    lightForA2 = !lightForA2;
+  }
 
-if(blow){
-  light = !light;
+  if(amountOfBlowingA3 < 600){//check to see if a3 is being blown ;)
+    lightForA3 = !lightForA3;
+  }
+
+  if(lightForA0){
+    digitalWrite(4,HIGH);
+  }
+  else{
+    digitalWrite(4,LOW);
+  }
+  if(lightForA1){
+    digitalWrite(5,HIGH);
+  }
+  else{
+    digitalWrite(5,LOW);
+  }
+  if(lightForA2){
+    digitalWrite(6,HIGH);
+  }
+  else{
+    digitalWrite(6,LOW);
+  }
+  if(lightForA3){
+    digitalWrite(7,HIGH);
+  }
+  else{
+    digitalWrite(7,LOW);
+  }
+  delay(250);
 }
-
-if(light){
-
-digitalWrite(2, HIGH);
-  delay(500);
-
-}else{
-digitalWrite(2, LOW);
-    delay(500);
- 
-}
-}
-
-
-
-
-
-
-
