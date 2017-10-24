ead500980ff7cb80b8786ea3e321f7961925bf9a
Jack Eller <jheguy2@gmail.com>
2017-10-24 13:36:38 -0400

    Now it's not illegible

--- candle/candle.ino
+++ candle/candle.ino
@@ -7,43 +7,42 @@ long duration;
 int distance;
 
 int getDistance(){
-int add[3];
-for(int x = 0; x < 3; x++){
-  digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-add[x] = duration*0.034/2;
+  int add[3];
+  for(int x = 0; x < 3; x++){
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    add[x] = duration*0.034/2;
+  }
+  return min(min(add[0],add[1]),min(add[1],add[2]));
 }
-return min(min(add[0],add[1]),min(add[1],add[2]));
-}
-
 
 void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
-pinMode(3, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+  pinMode(3, OUTPUT);
 }
 void loop() {
 
-distance = getDistance();
-
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
-if(distance < 20){
-
-  light = !light;
-  digitalWrite(3, light);
-  while(distance < 20){
-    distance = getDistance();
+  distance = getDistance();
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  if(distance < 20){
+  
+    light = !light;
+    digitalWrite(3, light);
+    while(distance < 20){
+      distance = getDistance();
+    }
+    Serial.println("OUT");
   }
-  Serial.println("OUT");
-}
 }
